785d375f5752312047fa90a58dc3f6d30c906190
Joseph Dorman <JDorman555@yahoo.com>
2023-08-12 22:51:30 -0500

    Add second test and fix Typo in first test of capitalize

--- jestPractice.test.js
+++ jestPractice.test.js
@@ -1,5 +1,9 @@
 import capitalize from "./jestPractice";
 
-test('Capitalize first letter', () => {
+test('Capitalize first letter (1)', () => {
   expect(capitalize('minecraft')).toBe('Minecraft');
+});
+
+test('Capitalize first letter (2)', () => {
+  expect(capitalize('valorant')).toBe('Valorant');
 });
