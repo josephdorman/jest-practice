fc6fb853fa66c5ff4535af884429e0910909af78
Joseph Dorman <JDorman555@yahoo.com>
2023-08-15 02:08:38 -0500

    Add caesarChipher test

--- jestPractice.test.js
+++ jestPractice.test.js
@@ -60,6 +60,10 @@ test('CaesarChipher capitals in a word', () => {
   expect(caesarChipher(5, 'MineCraft')).toBe('RnsjHwfky');
 });
 
+test('CaesarChipher capitals in a word', () => {
+  expect(caesarChipher(5, 'Zombies! Are scary? Whoa.')).toBe('Etrgnjx! Fwj Xhfwd? Bmtf.');
+});
+
 
 
 
